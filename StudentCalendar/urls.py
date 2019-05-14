06d8ca8a6f60208b5550e952e30d5ef19c820028
sr0di <49582301+sr0di@users.noreply.github.com>
2019-05-14 16:05:45 +0300

    Add files via upload

--- StudentCalendar/urls.py
+++ StudentCalendar/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('accounts.urls')),
+    path(r'admin/', admin.site.urls),
+    path(r'accounts/', include('accounts.urls')),
+    path(r'calendar/', include('cal.urls')),
 ]
